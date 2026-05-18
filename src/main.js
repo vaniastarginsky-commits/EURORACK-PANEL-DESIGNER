65ea98bf5c6b14febd623d0e7d6a683bec52e462
vaniastarginsky-commits <vania.starginsky@gmail.com>
2026-05-19 02:34:22 +0300

    Refactor panel designer into split runtime files

---
+++ src/main.js
@@ -0,0 +1,6 @@
+// Root render entrypoint.
+try {
+  const __panelDesignerRoot=ReactDOM.createRoot(document.getElementById('root'));__panelDesignerRoot.render(React.createElement(ErrorBoundary,null,React.createElement(AppProvider,null,React.createElement(App,null))));document.documentElement.setAttribute('data-panel-designer-ready','true');;
+} catch (err) {
+  window.renderPanelDesignerStartupError(err);
+}
